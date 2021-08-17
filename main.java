4230866d8e3e97d2e567526d25aab48c0a7da563
Rahul-K P <rahulkp01aug@gmail.com>
2021-08-17 14:56:42 +0530

    added process_input and padding text

--- main.java
+++ main.java
@@ -8,9 +8,43 @@ class AES {
         String text = buf.readLine();
         return(text);
     }
+    //slicing the given input into blocks of 128 binary bits 
+    static void process_input(String inp){
+        String a = "                ";
+        inp = inp + a.substring(0,(16-(inp.length()%16)));
+        int l = inp.length();
+        for(int i = 0; i < l -16; i=i+16)
+        {
+            print_matrix(create_matrix(inp.substring(i,i+16)));
+        }
+    }
+    //here the string input ha sto be 128 bits in binary 
+    static int[][] create_matrix(String inp) {
+        int text_matrix[][] = new int[4][4];
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                char ch = inp.charAt(i*4+j);
+                text_matrix[i][j] = (int)ch;  
+            }
+        }
+        return(text_matrix);
+    }
+    static void print_matrix(int arr[][]){
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j =0; j < 4; j++)
+            {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]) throws IOException {
         String plain_text, key_text;
         plain_text = input("plaintext");
-        key_text = input("key");
+        // key_text = input("key");
+        process_input(plain_text);
     }
 }
